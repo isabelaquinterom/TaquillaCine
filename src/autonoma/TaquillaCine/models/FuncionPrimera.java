8c3f4294e0d0cdda49d42de8be7600fbd3fa2ae6
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-04-03 22:36:25 -0500

    Mas clases agregadas

---
+++ src/autonoma/TaquillaCine/models/FuncionPrimera.java
@@ -0,0 +1,20 @@
+package autonoma.TaquillaCine.models;
+
+public class FuncionPrimera extends Funcion {
+    
+    public FuncionPrimera(String hora) {
+        super(hora);
+        calcularDescuentoFuncion();
+    }
+    
+    @Override
+    public void calcularDescuentoFuncion() {
+        // Las funciones de primera tienen un 5% de descuento
+        this.porcentajeDescuento = 0.05;
+    }
+    
+    @Override
+    public String toString() {
+        return "Funcion Primera - Hora: " + hora + " - Descuento: " + (porcentajeDescuento * 100) + "%";
+    }
+}
